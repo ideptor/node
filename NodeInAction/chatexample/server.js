73c6359ae41f7a04d56b4046adbe8a5f0e5bc7fd
Suhyun Kim <ideptor@gmail.com>
2019-12-03 14:43:37 +0900

    chat room - make folder structure

---
+++ NodeInAction/chatexample/server.js
@@ -0,0 +1,58 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('fs');
+var mime = require('mime');
+var cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type':'text/plain'});
+    response.write('Error 404: resource not found');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(
+        200,
+        {'Content-Type':mime.lookup(path.basename(filePath))}
+    )
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    if(cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    }
+    else {
+        fs.exists(absPath, (exists) => {
+            if(exists) {
+                fs.readFile(absPath, (err, data) => {
+                    if(err) {
+                        send404(response);
+                    } 
+                    else {
+                        cache[absPath] = data;
+                        sendFile(resonse, absPath, data);
+                    }
+                });
+            }
+            else {
+                send404(response);
+            }
+        });
+    }
+}
+
+var server = http.createServer((request, response)=> {
+    var filePath = false;
+    if(request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    var absPath = './'+filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
